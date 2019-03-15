44f8f6253ba8017c738f25c98e6283ad19f98d1b
junyoki <wnsgur6311@gmail.com>
2019-03-15 20:58:27 +0900

    Modify 조합 0의 개수 sol.cpp

--- Algorithm Basic/Math 1/16 조합 0의 개수/sol.cpp
+++ Algorithm Basic/Math 1/16 조합 0의 개수/sol.cpp
@@ -8,25 +8,37 @@ int main()
 
     cin >> inputN >> inputM;
 
-    for(long long i=2; i<inputN; i*=2)
+    for(long long i=2; i<=inputN; i*=2)
     {
         nCnt2 = nCnt2 + inputN/i;
     }
-    for(long long i=5; i<inputN; i*=5)
+    for(long long i=5; i<=inputN; i*=5)
     {
         nCnt5 = nCnt5 + inputN/i;
     }
 
-    for(long long i=2; i<inputN; i*=2)
+    for(long long i=2; i<=inputM; i*=2)
     {
-        mCnt2 = mCnt2 + inputN/i;
+        mCnt2 = mCnt2 + inputM/i;
     }
-    for(long long i=5; i<inputN; i*=5)
+    for(long long i=5; i<=inputM; i*=5)
     {
-        mCnt5 = mCnt5 + inputN/i;
+        mCnt5 = mCnt5 + inputM/i;
+    }
+
+    int tmp = inputN-inputM;
+
+    for(long long i=2; i<=tmp; i*=2)
+    {
+        mCnt2 = mCnt2 + tmp/i;
+    }
+    for(long long i=5; i<=tmp; i*=5)
+    {
+        mCnt5 = mCnt5 + tmp/i;
     }
 
     int resultCnt2 = nCnt2 - mCnt2, resultCnt5 = nCnt5 - mCnt5;
-    int result = (resultCnt2 > resultCnt5 ? resultCnt5 : resultCnt2)
+    int result = (resultCnt2 > resultCnt5 ? resultCnt5 : resultCnt2);
+	cout << result << endl;
     return 0;
 }
